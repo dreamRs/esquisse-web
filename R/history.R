3f3ac4f45f225d2bf1aac4cc2d8809d988a6244a
pvictor <victor.perrier@dreamRs.fr>
2025-01-31 17:49:14 +0100

    history placeholder

--- R/history.R
+++ R/history.R
@@ -15,7 +15,14 @@ history_server <- function(id, plot_list_r) {
 
       save_multi_ggplot_server(
         id = "history",
-        plot_list_r = plot_list_r
+        plot_list_r = plot_list_r,
+        placeholder = shinyWidgets::alert(
+          status = "info",
+          class = "col-12 fs-5",
+          ph("info", weight = "bold", height = "2em", vertical_align = "-0.5em"),
+          "There's no plot to display here, go back to esquisse and then save a plot by clicking the button on the top right.",
+          "All your saved plots will be available here."
+        )
       )
 
     }
